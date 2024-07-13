031aef10b00ba114dda3b98b355e9e71e754f03e
MrinalCom <Mrinal.Anand2021@iem.edu.in>
2024-07-14 03:24:58 +0530

    changes made

--- 05.How to setup a professional backend project/src/index.js
+++ 05.How to setup a professional backend project/src/index.js
@@ -1,30 +1,22 @@
-// require('dotenv').config({path:'./env'})
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-    path: "./env"
-})
-
-connectDB().then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running on port ${process.env.PORT}`)
-    })
-    app.on("error",(error)=>{
-        console.log(error);
-        throw error
-    })
-}
-
-).catch(()=>{
-    console.log(`MongoDB connection failed`)
+    path: './.env'
 })
 
 
 
-
-
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
@@ -37,24 +29,23 @@ connectDB().then(()=>{
 
 /*
 import express from "express"
-const app=express();
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
 
-(async ()=>{
-    try{
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error",(error)=>{
-        console.log("ERROR",error);
-        throw error
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
 
-       })
-       app.listen(process.env.PORT,()=>{
-        console.log(`Server is listening on PORT ${process.env.PORT }`);
-       })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
     }
-    catch(error){
-        console.error("ERROR",error)
-        throw error
-    }
-})() ---this thing is known as iife  function define and call at the same time
+})()
 
-*/ 
+*/
